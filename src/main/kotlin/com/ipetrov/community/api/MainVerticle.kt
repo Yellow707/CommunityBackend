142c8f062c90afcd6640f3e580b442ed768cbce5
Igor Petrov <fox0234@yandex.ru>
2020-04-13 08:20:30 +0300

    move MainVerticle file

---
+++ src/main/kotlin/com/ipetrov/community/api/MainVerticle.kt
@@ -0,0 +1,37 @@
+package com.ipetrov.community.api
+
+import com.google.gson.Gson
+import com.ipetrov.community.model.UserModel
+import io.vertx.core.AbstractVerticle
+import io.vertx.core.Future
+import io.vertx.core.Vertx
+
+fun main(args: Array<String>) {
+    val vertx = Vertx.vertx()
+    vertx.deployVerticle(MainVerticle())
+}
+
+class MainVerticle : AbstractVerticle() {
+
+    override fun start(startFuture: Future<Void>) {
+        super.start()
+
+        val server = vertx.createHttpServer()
+        server.webSocketHandler { handler ->
+            handler.writeTextMessage("Connected")
+            handler.handler{ msg ->
+                val user = Gson().fromJson(msg.toString(),UserModel::class.java)
+                val userString = user.toString()
+                print(user.name)
+            }
+
+        }.listen(8080) { http ->
+            if (http.succeeded()) {
+                startFuture.complete()
+                println("HTTP server started on port 8080")
+            } else {
+                startFuture.fail(http.cause());
+            }
+        }
+    }
+}
