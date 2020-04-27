05e40271b83255af3777661fc678de219f6b34d9
Igor Petrov <fox0234@yandex.ru>
2020-04-27 14:21:48 +0300

    remove test websocket connection

--- src/main/kotlin/com/ipetrov/community/api/MainVerticle.kt
+++ src/main/kotlin/com/ipetrov/community/api/MainVerticle.kt
@@ -9,23 +9,6 @@ class MainVerticle : AbstractVerticle() {
 
     override fun start(startFuture: Future<Void>) {
         super.start()
-
         val server = vertx.createHttpServer()
-        server.webSocketHandler { handler ->
-            handler.writeTextMessage("Connected")
-            handler.handler{ msg ->
-                val user = Gson().fromJson(msg.toString(),UserModel::class.java)
-                val userString = user.toString()
-                print(user.name)
-            }
-
-        }.listen(8080) { http ->
-            if (http.succeeded()) {
-                startFuture.complete()
-                println("HTTP server started on port 8080")
-            } else {
-                startFuture.fail(http.cause());
-            }
-        }
     }
 }
